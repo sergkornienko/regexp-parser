35ef6ee7dc3e9c9ac7d8415e5a2b8e6bf4dfe11d
serhiiko <serhiiko@serhiiko-mbp15.local>
2019-12-22 21:04:22 +0200

    move MONTHS to canstants

---
+++ constants.js
@@ -0,0 +1,49 @@
+exports.modules = [
+  {
+    eng: 'Jan',
+    rus: 'янв',
+  },
+  {
+    eng: 'Feb',
+    rus: 'февр',
+  },
+  {
+    eng: 'Mar',
+    rus: 'марта',
+  },
+  {
+    eng: 'Apr',
+    rus: 'апр',
+  },
+  {
+    eng: 'May',
+    rus: 'мая',
+  },
+  {
+    eng: 'Jun',
+    rus: 'июня',
+  },
+  {
+    eng: 'Jul',
+    rus: 'июля',
+  },
+  {
+    eng: 'Aug',
+    rus: 'авг',
+  }, 
+  {
+    eng: 'Sept',
+    rus: 'сент',
+  },
+  {
+    eng: 'Oct',
+    rus: 'окт',
+  }, {
+    eng: 'Nov',
+    rus: 'нояб',
+  },
+  {
+    eng: 'Dec',
+    rus: 'дек',
+  },
+];
